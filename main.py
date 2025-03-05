1142cd5bf042d1fec2b228122c4adf1ab9162976
Fazzelis <odidk08@gmail.com>
2025-03-05 11:47:15 +0700

    Подключил свою локальную бд и внес правки в endpoint регистрации

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     hashed_password = get_password_hash(user.password)
-    db_user = create_user(db, email=user.email, hashed_password=hashed_password)
+    db_user = create_user(db, user=user)
     return {"message": "User created successfully"}
 
 # Аутентификация
